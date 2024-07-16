0533d5ceaeefc79424a8ad0653ee96a30f7adf7a
lowe <diegocorrea.cc@gmail.com>
2024-07-16 15:19:39 -0300

    first commit

--- shell/food_opt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ./pierre_in_frame &&
-python3 step1_preprocessing.py from_file=YES file_name=food &&
-python3 step2_searches.py from_file=YES file_name=food
--- shell/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ./pierre_in_frame &&
-python3 step6_protocol.py from_file=YES file_name=step6 &&
-python3 step7_charts_tables.py from_file=YES file_name=step7
