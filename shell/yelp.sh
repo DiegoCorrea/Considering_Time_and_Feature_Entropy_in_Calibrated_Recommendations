4f5451ab2d57f7717c1a21f1ee38b8005d64b3fa
diego <diego.correa@ufba.br>
2024-10-22 17:06:44 +0200

    yelp steps

--- shell/yelp.sh
+++ shell/yelp.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+cd ./pierre_in_frame &&
 python3 step3_processing.py from_file=YES file_name=yelp > ../logs/step3_yelp.log &&
 python3 step4_postprocessing.py from_file=YES file_name=yelp > ../logs/step4_yelp.log &&
 python3 step5_metrics.py from_file=YES file_name=yelp > ../logs/step5_yelp.log
