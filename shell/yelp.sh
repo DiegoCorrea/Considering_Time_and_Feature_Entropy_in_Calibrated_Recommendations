852eb841c060215d65594b3287cd9ce155c9f9d1
diego <diego.correa@ufba.br>
2024-10-24 17:05:50 +0200

    new yelp run

--- shell/yelp.sh
+++ shell/yelp.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 cd ./pierre_in_frame &&
+python3 step1_preprocessing.py from_file=YES file_name=yelp &&
+python3 step1_preprocessing.py from_file=YES file_name=yelp_distribution &&
+python3 step1_preprocessing.py from_file=YES file_name=yelp_analyze &&
+
+python3 step2_searches.py from_file=YES file_name=yelp &&
+
 python3 step3_processing.py from_file=YES file_name=yelp > ../logs/step3_yelp.log &&
 python3 step4_postprocessing.py from_file=YES file_name=yelp > ../logs/step4_yelp.log &&
 python3 step5_metrics.py from_file=YES file_name=yelp > ../logs/step5_yelp.log
